a748f2d78c0778972beb46cee8d8b8818a8ed8b0
Emanuel Jesus Figueredo <emanuel.j.figueredo@gmail.com>
2025-04-03 04:55:20 -0300

    Migrado a MySQL con pool de conexiones

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const pool = require('../db'); // Ahora usamos un pool de conexiones
 // Ruta para obtener todos los productos
 router.get('/', async (req, res) => {
     try {
-        const [results] = await pool.promise().query('SELECT * FROM productos');
+        const [results] = await pool.query('SELECT * FROM productos');
         res.json(results);
     } catch (err) {
         console.error('Error en la consulta:', err.message);
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 router.get('/:tipo', async (req, res) => {
     try {
         const { tipo } = req.params;
-        const [results] = await pool.promise().query('SELECT * FROM productos WHERE tipo = ?', [tipo]);
+        const [results] = await pool.query('SELECT * FROM productos WHERE tipo = ?', [tipo]);
         res.json(results);
     } catch (err) {
         console.error('Error en la consulta:', err.message);
